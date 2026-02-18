0ad1e2d7bf12bb9f4f8c1707e8d4482f3a99933b
Miłosz Osadczuk <m2ilosz@outlook.com>
2026-02-18 17:16:56 +0100

    AutoSizeIframe fix

--- js/ui/controls/AutoSizeIframe.js
+++ js/ui/controls/AutoSizeIframe.js
@@ -1,25 +1,21 @@
-
 export class AutoSizeIframe extends HTMLIFrameElement {
-
 
     constructor() {
         super();
 
-        this.addEventListener("load", (event) => {
+        this.addEventListener("load", (event) => { 
             new ResizeObserver((entries) => {
-                this.height = this.contentDocument.documentElement.scrollHeight;
-                this.width = this.contentDocument.documentElement.scrollWidth;
+                this.setSize();
             }).observe(this.contentDocument.body);
         });
     }
-    
-    connectedCallback() {
 
+    setSize() {
+        let bounds = this.contentDocument.documentElement.getBoundingClientRect();
+
+        this.height = Math.ceil(bounds.height - bounds.y);
+        this.width = Math.ceil(bounds.width - bounds.x);
     }
-
-
 }
-
-
 
 customElements.define("auto-size", AutoSizeIframe, {extends: "iframe"});
